2f614820815af6d13bd683e8ba718f82297f5354
Corey Osman <corey@logicminds.biz>
2013-07-10 01:41:00 -0700

    added fru spec code and reworked how fru data is returned

--- spec/unit/ipmitool/fru_spec.rb
+++ spec/unit/ipmitool/fru_spec.rb
@@ -20,8 +20,39 @@ describe :Fru do
     @fru.getfrus.should_not be_nil
   end
 
+
   it "should return a list of parsed frus" do
-    @fru.list.count.should be(20)
+    @fru.list.count.should eq(13)
+  end
+
+  it 'should return a list of parsed frus even if one fru exists' do
+    data = nil
+    File.open('spec/fixtures/ipmitool/hp_dl380_g5_fru.txt','r') do |file|
+      data = file.read
+    end
+    @fru.stub(:getfrus).and_return(data)
+    @fru.list.count.should eq(1)
+
+  end
+
+  it 'should return a manufactor' do
+    @fru.product_manufacturer.should eq('HP')
+  end
+
+  it 'should return a product' do
+    @fru.product_name.should eq('ProLiant SL230s Gen8')
+  end
+
+  it 'should return a board serial' do
+    @fru.board_serial.should eq('USE238F0D0')
+  end
+
+  it 'should return a product serial' do
+    @fru.product_serial.should eq('USE238F0D0')
+  end
+
+  it 'should return a asset tag' do
+    @fru.product_asset_tag.should eq('000015B90F82')
   end
 
 
