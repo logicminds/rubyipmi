38704bca91c10918eb4546cd0320ce52ea1ff7dc
Brandon Dunne <bdunne@redhat.com>
2015-09-22 16:47:06 -0400

    Fix rubocop Style/MethodName "Use snake_case for method names."
    Rename private method
    Add deprecation warnings for two public methods
    Delete unused method
    Update .rubocop.yml accordingly
    Refactor duplicated chassis power command methods

--- lib/rubyipmi/ipmitool/errorcodes.rb
+++ lib/rubyipmi/ipmitool/errorcodes.rb
@@ -28,28 +28,6 @@ module Rubyipmi
         end
         raise "No Fix found" if fix.nil?
       end
-
-      def throwError
-        # Find out what kind of error is happening, parse results
-        # Check for authentication or connection issue
-
-        if @result =~ /timeout|timed\ out/
-          code = "ipmi call: #{@lastcall} timed out"
-          raise code
-        else
-          code = @result.split(":").last.chomp.strip unless @result.empty?
-        end
-        case code
-        when "invalid hostname"
-          raise code
-        when "password invalid"
-          raise code
-        when "username invalid"
-          raise code
-        else
-          raise :ipmierror, code
-        end
-      end
     end
   end
 end
