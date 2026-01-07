logicminds/rubyipmi

commit 7668b82175498743ff80bae133b6d31bd887ebd9

Fix tests for Ruby 3.0

lib/rubyipmi/commands/basecommand.rb
@@ -37,9 +37,8 @@ module Rubyipmi
     end
 
     def locate_command(commandname)
-      location = `which #{commandname}`.strip
-      unless $?.success?
-        logger.error("#{commandname} command not found, is #{commandname} installed?") if logger
+      unless location = Rubyipmi.locate_command(commandname)
+        logger&.error("#{commandname} command not found, is #{commandname} installed?")
         raise "#{commandname} command not found, is #{commandname} installed?"
       end
       location
@@ -67,10 +66,10 @@ module Rubyipmi
       logger.debug(makecommand) if logger
       begin
         command = makecommand
-        @lastcall = "#{command}"
-        @result = `#{command} 2>&1`
+        @lastcall = command.to_s
+        @result, @result_err, status = Rubyipmi.capture3(command)
         # sometimes the command tool does not return the correct result, validate it with additional code
-        process_status = validate_status($?)
+        process_status = validate_status(status)
       rescue
         if retrycount < max_retry_count
           find_fix(@result)
@@ -96,7 +95,8 @@ module Rubyipmi
       begin
         fix = ErrorCodes.search(result)
         @options.merge_notify!(fix)
-      rescue
+      rescue e
+        raise e
         Rubyipmi.logger.debug("Could not find fix for error code: \n#{result}") if logger
         raise "Could not find fix for error code: \n#{result}"
       end
@@ -108,7 +108,8 @@ module Rubyipmi
 
     # This method will check if the results are really valid as the exit code can be misleading and incorrect
     def validate_status(exitstatus)
-      raise "Error occurred" unless exitstatus.success?
+      raise "Error occurred" unless exitstatus
+      raise "Error occurred" if exitstatus.is_a?(Process::Status) && exitstatus.success?
 
       true
     end

spec/unit/freeipmi/bmc-info_spec.rb
@@ -20,8 +20,7 @@ describe "Bmc" do
       data = file.read
     end
     allow(@bmcinfo).to receive(:locate_command).with('bmc-info').and_return("#{@path}/bmc-info")
-    allow(@bmcinfo).to receive(:`).and_return(data)
-    allow($CHILD_STATUS).to receive(:success?).and_return(true)
+    allow(Rubyipmi).to receive(:capture3).and_return([data, '', true])
   end
 
   it "cmd should be bmc-info with correct number of arguments" do

spec/unit/freeipmi/fru_spec.rb
@@ -21,7 +21,7 @@ describe :Fru do
 
     allow(@fru).to receive(:locate_command).with('ipmi-fru').and_return("#{@path}/ipmi-fru")
     allow(@fru).to receive(:`).and_return(data)
-    allow($CHILD_STATUS).to receive(:success?).and_return(true)
+    allow(Rubyipmi).to receive(:capture3).and_return([data, '', true])
   end
 
   it "cmd should be ipmi-fru with correct number of arguments" do

spec/unit/freeipmi/sensors_spec.rb
@@ -24,8 +24,7 @@ describe :Sensors do
     allow(@sensors).to receive(:locate_command).with('ipmi-sensors').and_return('/usr/local/bin/ipmi-sensors')
 
     # these stubs allow us to run the command and return the fixtures
-    allow(@sensors).to receive(:`).and_return(data)
-    allow($CHILD_STATUS).to receive(:success?).and_return(true)
+    allow(Rubyipmi).to receive(:capture3).and_return([data, '', true])
   end
 
   it "cmd should be ipmi-sensors with six arguments" do

spec/unit/ipmitool/bmc_spec.rb
@@ -20,8 +20,7 @@ describe "Bmc" do
     end
 
     allow(@bmc).to receive(:locate_command).with('ipmitool').and_return("#{@path}/ipmitool")
-    allow(@bmc).to receive(:`).and_return(data)
-    allow($CHILD_STATUS).to receive(:success?).and_return(true)
+    allow(Rubyipmi).to receive(:capture3).and_return([data, '', true])
 
     allow(@bmc).to receive(:guid).and_return("guid")
   end

spec/unit/ipmitool/fru_spec.rb
@@ -22,8 +22,7 @@ describe :Fru do
     end
 
     allow(@fru).to receive(:locate_command).with('ipmitool').and_return("#{@path}/ipmitool")
-    allow(@fru).to receive(:`).and_return(data)
-    allow($CHILD_STATUS).to receive(:success?).and_return(true)
+    allow(Rubyipmi).to receive(:capture3).and_return([data, '', true])
   end
 
   it "cmd should be ipmi-sensors with correct number of arguments" do

spec/unit/ipmitool/lan_spec.rb
@@ -20,8 +20,7 @@ describe "Lan" do
     end
 
     allow(@lan).to receive(:locate_command).with('ipmitool').and_return("#{@path}/ipmitool")
-    allow(@lan).to receive(:`).and_return(data)
-    allow($CHILD_STATUS).to receive(:success?).and_return(true)
+    allow(Rubyipmi).to receive(:capture3).and_return([data, '', true])
   end
 
   it "cmd should be lan with correct number of arguments" do

spec/unit/ipmitool/sensors_spec.rb
@@ -20,11 +20,7 @@ describe :Sensors do
     File.open("spec/fixtures/#{provider}/sensors.txt", 'r') do |file|
       data = file.read
     end
-    allow(@sensors).to receive(:locate_command).with('ipmitool').and_return("#{@path}/ipmitool")
-    allow(@sensors).to receive(:`).and_return(data)
-
-    # this is causing an error: An expectation of :success? was set on nil
-    allow($CHILD_STATUS).to receive(:success?).and_return(true)
+    allow(Rubyipmi).to receive(:capture3).and_return([data, '', true])
   end
 
   # it 'should figure out to add the -I lanplus' do

commit 6061b9c02ca6e2228f3ce842ab32661c5b96f9a4

Properly mock capture3 API which returns a Process::Status, not true
Fixes: 7668b82175498743ff80bae133b6d31bd887ebd9

lib/rubyipmi/commands/basecommand.rb
@@ -108,7 +108,7 @@ module Rubyipmi
 
     # This method will check if the results are really valid as the exit code can be misleading and incorrect
     def validate_status(exitstatus)
-      raise "Error occurred" unless exitstatus.is_a?(Process::Status) && exitstatus.success?
+      raise "Error occurred" unless exitstatus.respond_to?(:success?) && exitstatus.success?
 
       true
     end

spec/spec_helper.rb
@@ -40,6 +40,10 @@ def verify_ipmitool_command(cmdobj, exp_args_count, expcmd, required_args)
   expect(args_match.count).to eq(exp_args_count)
 end
 
+def mock_success_status
+  instance_double(Process::Status, success?: true)
+end
+
 RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = [:expect]

spec/unit/freeipmi/bmc-info_spec.rb
@@ -20,7 +20,7 @@ describe "Bmc" do
       data = file.read
     end
     allow(@bmcinfo).to receive(:locate_command).with('bmc-info').and_return("#{@path}/bmc-info")
-    allow(Rubyipmi).to receive(:capture3).and_return([data, '', true])
+    allow(Rubyipmi).to receive(:capture3).and_return([data, '', mock_success_status])
   end
 
   it "cmd should be bmc-info with correct number of arguments" do

spec/unit/freeipmi/fru_spec.rb
@@ -21,7 +21,7 @@ describe :Fru do
 
     allow(@fru).to receive(:locate_command).with('ipmi-fru').and_return("#{@path}/ipmi-fru")
     allow(@fru).to receive(:`).and_return(data)
-    allow(Rubyipmi).to receive(:capture3).and_return([data, '', true])
+    allow(Rubyipmi).to receive(:capture3).and_return([data, '', mock_success_status])
   end
 
   it "cmd should be ipmi-fru with correct number of arguments" do

spec/unit/freeipmi/sensors_spec.rb
@@ -24,7 +24,7 @@ describe :Sensors do
     allow(@sensors).to receive(:locate_command).with('ipmi-sensors').and_return('/usr/local/bin/ipmi-sensors')
 
     # these stubs allow us to run the command and return the fixtures
-    allow(Rubyipmi).to receive(:capture3).and_return([data, '', true])
+    allow(Rubyipmi).to receive(:capture3).and_return([data, '', mock_success_status])
   end
 
   it "cmd should be ipmi-sensors with six arguments" do

spec/unit/ipmitool/bmc_spec.rb
@@ -20,7 +20,7 @@ describe "Bmc" do
     end
 
     allow(@bmc).to receive(:locate_command).with('ipmitool').and_return("#{@path}/ipmitool")
-    allow(Rubyipmi).to receive(:capture3).and_return([data, '', true])
+    allow(Rubyipmi).to receive(:capture3).and_return([data, '', mock_success_status])
 
     allow(@bmc).to receive(:guid).and_return("guid")
   end

spec/unit/ipmitool/fru_spec.rb
@@ -22,7 +22,7 @@ describe :Fru do
     end
 
     allow(@fru).to receive(:locate_command).with('ipmitool').and_return("#{@path}/ipmitool")
-    allow(Rubyipmi).to receive(:capture3).and_return([data, '', true])
+    allow(Rubyipmi).to receive(:capture3).and_return([data, '', mock_success_status])
   end
 
   it "cmd should be ipmi-sensors with correct number of arguments" do

spec/unit/ipmitool/lan_spec.rb
@@ -20,7 +20,7 @@ describe "Lan" do
     end
 
     allow(@lan).to receive(:locate_command).with('ipmitool').and_return("#{@path}/ipmitool")
-    allow(Rubyipmi).to receive(:capture3).and_return([data, '', true])
+    allow(Rubyipmi).to receive(:capture3).and_return([data, '', mock_success_status])
   end
 
   it "cmd should be lan with correct number of arguments" do

spec/unit/ipmitool/sensors_spec.rb
@@ -20,7 +20,7 @@ describe :Sensors do
     File.open("spec/fixtures/#{provider}/sensors.txt", 'r') do |file|
       data = file.read
     end
-    allow(Rubyipmi).to receive(:capture3).and_return([data, '', true])
+    allow(Rubyipmi).to receive(:capture3).and_return([data, '', mock_success_status])
   end
 
   # it 'should figure out to add the -I lanplus' do