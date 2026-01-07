logicminds/rubyipmi

commit 7668b82175498743ff80bae133b6d31bd887ebd9

Fix tests for Ruby 3.0

lib/rubyipmi/commands/basecommand.rb
@@ -37,9 +37,8 @@ module Rubyipmi
     end
 
     def locate_command(commandname)
-      location = `which #{commandname}`.strip
-      unless $?.success?
-        logger.error("#{commandname} command not found, is #{commandname} installed?") if logger
+      unless location = Rubyipmi.locate_command(commandname)
+        logger&.error("#{commandname} command not found, is #{commandname} installed?")
         raise "#{commandname} command not found, is #{commandname} installed?"
       end
       location
@@ -67,10 +66,10 @@ module Rubyipmi
       logger.debug(makecommand) if logger
       begin
         command = makecommand
-        @lastcall = "#{command}"
-        @result = `#{command} 2>&1`
+        @lastcall = command.to_s
+        @result, @result_err, status = Rubyipmi.capture3(command)
         # sometimes the command tool does not return the correct result, validate it with additional code
-        process_status = validate_status($?)
+        process_status = validate_status(status)
       rescue
         if retrycount < max_retry_count
           find_fix(@result)
@@ -96,7 +95,8 @@ module Rubyipmi
       begin
         fix = ErrorCodes.search(result)
         @options.merge_notify!(fix)
-      rescue
+      rescue e
+        raise e
         Rubyipmi.logger.debug("Could not find fix for error code: \n#{result}") if logger
         raise "Could not find fix for error code: \n#{result}"
       end
@@ -108,7 +108,8 @@ module Rubyipmi
 
     # This method will check if the results are really valid as the exit code can be misleading and incorrect
     def validate_status(exitstatus)
-      raise "Error occurred" unless exitstatus.success?
+      raise "Error occurred" unless exitstatus
+      raise "Error occurred" if exitstatus.is_a?(Process::Status) && exitstatus.success?
 
       true
     end

lib/rubyipmi.rb
@@ -19,6 +19,7 @@
 require 'rubyipmi/ipmitool/connection'
 require 'rubyipmi/freeipmi/connection'
 require 'logger'
+require 'open3'
 
 class NullLogger < Logger
   def initialize(*_args)
@@ -144,11 +145,18 @@ module Rubyipmi
     PRIV_TYPES.include?(type)
   end
 
+  # test-friendly capture3
+  def self.capture3(cmd)
+    return Open3.capture3(cmd)
+  end
+
   # method used to find the command which also makes it easier to mock with
   def self.locate_command(commandname)
-    location = `which #{commandname}`.strip
-    location = nil unless $?.success?
-    location
+    stdout, stderr, status = Open3.capture3("which #{commandname}")
+    logger&.error("Which command returned: #{stderr}") unless status.success?
+
+    return nil unless status.success?
+    stdout
   end
 
   # Return true or false if the provider is available

rubyipmi.gemspec
@@ -21,7 +21,7 @@ Gem::Specification.new do |s|
   s.license = "LGPL-2.1-only"
   s.summary = "A ruby wrapper for ipmi command line tools that supports ipmitool and freeipmi"
   s.require_paths = ['lib']
-  s.add_development_dependency 'rspec', "~> 3.1"
+  s.add_development_dependency 'rspec'
   s.add_development_dependency 'rdoc', "~> 3.12"
   s.add_development_dependency 'bundler', "~> 2.0"
   s.add_development_dependency 'highline', '>= 1.0', '< 3'

spec/unit/freeipmi/bmc-info_spec.rb
@@ -20,8 +20,7 @@ describe "Bmc" do
       data = file.read
     end
     allow(@bmcinfo).to receive(:locate_command).with('bmc-info').and_return("#{@path}/bmc-info")
-    allow(@bmcinfo).to receive(:`).and_return(data)
-    allow($CHILD_STATUS).to receive(:success?).and_return(true)
+    allow(Rubyipmi).to receive(:capture3).and_return([data, '', true])
   end
 
   it "cmd should be bmc-info with correct number of arguments" do

spec/unit/freeipmi/fru_spec.rb
@@ -21,7 +21,7 @@ describe :Fru do
 
     allow(@fru).to receive(:locate_command).with('ipmi-fru').and_return("#{@path}/ipmi-fru")
     allow(@fru).to receive(:`).and_return(data)
-    allow($CHILD_STATUS).to receive(:success?).and_return(true)
+    allow(Rubyipmi).to receive(:capture3).and_return([data, '', true])
   end
 
   it "cmd should be ipmi-fru with correct number of arguments" do

spec/unit/freeipmi/sensors_spec.rb
@@ -24,8 +24,7 @@ describe :Sensors do
     allow(@sensors).to receive(:locate_command).with('ipmi-sensors').and_return('/usr/local/bin/ipmi-sensors')
 
     # these stubs allow us to run the command and return the fixtures
-    allow(@sensors).to receive(:`).and_return(data)
-    allow($CHILD_STATUS).to receive(:success?).and_return(true)
+    allow(Rubyipmi).to receive(:capture3).and_return([data, '', true])
   end
 
   it "cmd should be ipmi-sensors with six arguments" do

spec/unit/ipmitool/bmc_spec.rb
@@ -20,8 +20,7 @@ describe "Bmc" do
     end
 
     allow(@bmc).to receive(:locate_command).with('ipmitool').and_return("#{@path}/ipmitool")
-    allow(@bmc).to receive(:`).and_return(data)
-    allow($CHILD_STATUS).to receive(:success?).and_return(true)
+    allow(Rubyipmi).to receive(:capture3).and_return([data, '', true])
 
     allow(@bmc).to receive(:guid).and_return("guid")
   end

spec/unit/ipmitool/fru_spec.rb
@@ -22,8 +22,7 @@ describe :Fru do
     end
 
     allow(@fru).to receive(:locate_command).with('ipmitool').and_return("#{@path}/ipmitool")
-    allow(@fru).to receive(:`).and_return(data)
-    allow($CHILD_STATUS).to receive(:success?).and_return(true)
+    allow(Rubyipmi).to receive(:capture3).and_return([data, '', true])
   end
 
   it "cmd should be ipmi-sensors with correct number of arguments" do

commit 5acf49180a8d4f672703cd6d0cb2dd391aacd873

Don't re-raise the original exception in basecommand
Otherwise we can't log our error properly

Fixes: 7668b82175498743ff80bae133b6d31bd887ebd9

lib/rubyipmi/commands/basecommand.rb
@@ -95,8 +95,7 @@ module Rubyipmi
       begin
         fix = ErrorCodes.search(result)
         @options.merge_notify!(fix)
-      rescue e
-        raise e
+      rescue
         Rubyipmi.logger.debug("Could not find fix for error code: \n#{result}") if logger
         raise "Could not find fix for error code: \n#{result}"
       end